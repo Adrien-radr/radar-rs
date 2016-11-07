56eb64d6b8d3f9e990ba89408670dd398388b6c9
Adrian Revk <ra.dubouchet@gmail.com>
2016-11-06 21:51:09 -0800

    - Mouse state - Moved everything to context.rs

--- src/context.rs
+++ src/context.rs
@@ -5,6 +5,7 @@ use std::sync::mpsc;
 use self::glfw::Context as glfwContext;
 
 use system::config;
+use math::vec4::Vec4;
 
 pub struct Context {
     pub glfw: glfw::Glfw,
@@ -18,7 +19,12 @@ pub struct Context {
     config: config::Config,
 
     key_state: Vec<bool>,
-    prev_key_state: Vec<bool>
+    prev_key_state: Vec<bool>,
+    mouse_state: Vec<bool>,
+    prev_mouse_state: Vec<bool>,
+
+
+    clear_color: Vec4
 }
 
 impl Context {
@@ -39,7 +45,7 @@ impl Context {
             .expect("Failed to create GLFW window.");
 
         window.make_current();
-        window.set_key_polling(true);
+        window.set_all_polling(true);
 
         Context::load_gl_procs(&mut window);
 
@@ -51,6 +57,8 @@ impl Context {
             gl::Viewport(0, 0, winw as i32, winh as i32);
         }
 
+        let default_clear_color = Vec4::new(0.2, 0.2, 0.2, 1.0);
+
         Context {
             glfw: ctx,
             events: events,
@@ -61,9 +69,12 @@ impl Context {
             window_height: winh,
 
             config: conf,
+            key_state: vec![false; 1024],
+            prev_key_state: vec![false; 1024],
+            mouse_state: vec![false; 16],
+            prev_mouse_state: vec![false; 16],
 
-            key_state: vec![false; 256],
-            prev_key_state: vec![false; 256]
+            clear_color: default_clear_color
         }
     }
 
@@ -79,6 +90,12 @@ impl Context {
                 glfw::WindowEvent::Key(k, _, glfw::Action::Release, _) => {
                     self.key_state[k as usize] = false;
                 }
+                glfw::WindowEvent::MouseButton(mb, glfw::Action::Press, _) => {
+                    self.mouse_state[mb as usize] = true;
+                }
+                glfw::WindowEvent::MouseButton(mb, glfw::Action::Release, _) => {
+                    self.mouse_state[mb as usize] = false;
+                }
                 _ => {}
             }
         }
@@ -86,8 +103,8 @@ impl Context {
 
     fn handle_events(&mut self) {
         // cpy last frame events as previous state
-        // array_mut_ref![self.prev_key_state; 0; 256] = *array_ref![self.key_state; 0; 256];
         self.prev_key_state.clone_from(&self.key_state);
+        self.prev_mouse_state.clone_from(&self.mouse_state);
 
         self.glfw.poll_events();
 
@@ -98,14 +115,61 @@ impl Context {
         self.handle_window_events(&evts);
     }
 
+    pub fn is_key_hit(&self, key: glfw::Key) -> bool {
+        self.key_state[key as usize] == true && self.prev_key_state[key as usize] == false
+    }
+
+    pub fn is_key_released(&self, key: glfw::Key) -> bool {
+        self.key_state[key as usize] == false && self.prev_key_state[key as usize] == true
+    }
+
+    pub fn is_key_down(&self, key: glfw::Key) -> bool {
+        self.key_state[key as usize] == true
+    }
+
+    pub fn is_mouse_hit(&self, mb: glfw::MouseButton) -> bool {
+        self.mouse_state[mb as usize] == true && self.prev_mouse_state[mb as usize] == false
+    }
+
+    pub fn is_mouse_released(&self, mb: glfw::MouseButton) -> bool {
+        self.mouse_state[mb as usize] == false && self.prev_mouse_state[mb as usize] == true
+    }
+
+    pub fn is_mouse_down(&self, mb: glfw::MouseButton) -> bool {
+        self.mouse_state[mb as usize] == true
+    }
+
+    fn clear_buffers(&self) {
+        unsafe {
+            gl::ClearColor(self.clear_color.x, self.clear_color.y, self.clear_color.z, self.clear_color.w);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        !self.window.should_close()
+    }
     pub fn run(&mut self) {
         self.handle_events();
 
-        if self.key_state[glfw::Key::A as usize] == true && self.prev_key_state[glfw::Key::A as usize] == false {
-            println!("Key A hit");
+        if self.is_key_down(glfw::Key::A) {
+            let newr = self.clear_color.x - 0.01;
+            if newr > 0.0 {
+                self.clear_color.x = newr;
+            } else {
+                self.clear_color.x = 0.0;
+            }
         }
-        if self.key_state[glfw::Key::A as usize] == false && self.prev_key_state[glfw::Key::A as usize] == true {
-            println!("Key A released");
+        if self.is_key_down(glfw::Key::D) {
+            let newr = self.clear_color.x + 0.01;
+            if newr < 1.0 {
+                self.clear_color.x = newr;
+            } else {
+                self.clear_color.x = 1.0;
+            }
         }
+
+        self.clear_buffers();
+        self.window.swap_buffers();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,13 @@
-extern crate glfw;
-extern crate gl;
-
-// use glfw::{Action, Context, Key};
-use gl::types::*;
-use glfw::Context as glfwContext;
-
-mod math;
-mod system;
 mod context;
+mod system;
+mod math;
 
-use math::vec3::Vec3;
 use context::Context;
 
 fn main() {
     let mut ctx = Context::new("data/config.json");
-    let mut bck_col = Vec3::new(1.0, 0.8, 0.05);
 
-    while !ctx.window.should_close() {
+    while ctx.is_running() {
         ctx.run();
-
-        unsafe {
-            gl::ClearColor(bck_col.x, bck_col.y, bck_col.z, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-        }
-
-        // WTF RUST ??? NO MAX FOR F32 !?!??!
-        if ctx.window.get_key(glfw::Key::A) == glfw::Action::Press {
-            let newr = bck_col.x - 0.01;
-            if newr > 0.0 {
-                bck_col.x = newr;
-            } else {
-                bck_col.x = 0.0;
-            }
-        }
-        if ctx.window.get_key(glfw::Key::D) == glfw::Action::Press {
-            let newr = bck_col.x + 0.01;
-            if newr < 1.0 {
-                bck_col.x = newr;
-            } else {
-                bck_col.x = 1.0;
-            }
-        }
-        ctx.window.swap_buffers();
-
     }
 }
