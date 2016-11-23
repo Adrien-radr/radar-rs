79d5eb3e4e7866d7871f9a4c83778f9a5fbba75c
Alexandre Jubert <Alexandre Jubert>
2016-11-23 00:26:07 -0500

    shader.rs cleaning

--- src/renderer/shader.rs
+++ src/renderer/shader.rs
@@ -118,7 +118,7 @@ impl Program {
 }
 
 
-pub fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+fn compile_shader(src: &str, ty: GLenum) -> GLuint {
     let shader;
     unsafe {
         shader = gl::CreateShader(ty);
@@ -140,26 +140,4 @@ pub fn compile_shader(src: &str, ty: GLenum) -> GLuint {
         }
     }
     shader
-}
-
-pub fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
-    unsafe {
-        let program = gl::CreateProgram();
-        gl::AttachShader(program, vs);
-        gl::AttachShader(program, fs);
-        gl::LinkProgram(program);
-
-        let mut status = gl::FALSE as GLint;
-        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
-
-        if status != (gl::TRUE as GLint) {
-            let mut len: GLint = 0;
-            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
-            let mut buf = Vec::with_capacity(len as usize);
-            buf.set_len((len as usize) - 1); // no trailing char
-            gl::GetProgramInfoLog(program, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-            panic!("{}", str::from_utf8(&buf).ok().expect("ProgramInfoLog not valid utf8"));
-        }
-        program
-    }
 }
